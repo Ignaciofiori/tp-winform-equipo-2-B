1c4b7b712d494c613e17ea15c747918fac748a82
Ignacio Fiori <nacho.fiori@acqua.net.ar>
2025-09-07 19:35:49 -0300

    Primer Commit, con estructura Inicial de Modelado de Capas

---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using modelo;
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {   //primer Metodo Vacio del articuloNegocio
+        public List<Articulo> listarArticulos()
+        {
+            List < Articulo >  listaArticulos = new List<Articulo>();
+
+            return listaArticulos;
+        }
+    }
+}
