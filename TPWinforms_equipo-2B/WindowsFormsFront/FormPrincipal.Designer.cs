2b3255c2a7cf0aa0b46260d0982fa4f9a069d900
Ignacio Fiori <nacho.fiori@acqua.net.ar>
2025-09-09 00:00:34 -0300

    restructuracion del proyecto a solucion como corresponde, (cree proyecto de nuevo con el contenido que aportaron debido a un error mio al crear el proyecto del repo). Se crea AccesoDatos en Negocio para conexion e interaccion con base de datos. Se agrega en Negocio CategoriaNegocio con la primer funcionalidad listarCategorias(). En el FormPrincipal se puede ver la prueba de este metodo.

---
+++ TPWinforms_equipo-2B/WindowsFormsFront/FormPrincipal.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsFront
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCategorias = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCategorias
+            // 
+            this.dgvCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategorias.Location = new System.Drawing.Point(89, 88);
+            this.dgvCategorias.Name = "dgvCategorias";
+            this.dgvCategorias.RowHeadersWidth = 51;
+            this.dgvCategorias.RowTemplate.Height = 24;
+            this.dgvCategorias.Size = new System.Drawing.Size(359, 150);
+            this.dgvCategorias.TabIndex = 0;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvCategorias);
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplicacion de Gestion: ";
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCategorias;
+    }
+}
+
---
+++ TPWinforms_equipo-2B/WindowsFormsFront/FormPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using modelo;
+using negocio;
+
+namespace WindowsFormsFront
+{
+    public partial class FormPrincipal : Form
+    {
+        private List<Categoria> listaCategorias;
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+            CategoriaNegocio negocioCategoria = new CategoriaNegocio();
+            listaCategorias = negocioCategoria.listarCategorias();
+            dgvCategorias.DataSource = listaCategorias;
+        }
+    }
+}
